3d4078151afcf88ac0ee1fdeb400bec04eb241ff
Chaklam <chaklam@ait.asia>
2022-11-17 17:31:14 +0700

    update

---
+++ Lectures/06 - Deployment/code-along/app.py
@@ -0,0 +1,15 @@
+#the main file containing the FastAPI
+
+#these are the files we just made
+import ml.classifier as clf
+from schema.iris import Iris
+
+#these are other libs
+from joblib import load
+from fastapi import FastAPI
+
+#1. define the fastapi instance
+
+#2. define the startup event
+
+#3. define the url for prediction
